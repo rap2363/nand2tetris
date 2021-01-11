ed0f17acb02001fb038a39df77b116cea5e9b3a3
Rohan Paranjpe <rap2363@gmail.com>
2021-01-11 09:35:19 -0800

    Projects 2, 3, and 4

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -6,9 +6,63 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+  @8192
+  D=A
+  @numrows
+  M=D // numrows=8192 (the number of rows in the screen)
+
+(START)
+  @offset
+  M=0 // Set the offset to 0
+  @KBD
+  D=M
+  @WHITE
+  D;JEQ // If no key has been pressed, set the color value to white.
+  @BLACK
+  0;JMP // Otherwise we color everything black.
+
+(WHITE)
+  @color
+  D=M
+  M=0 // Set the color value to white
+  D=D-M
+  @START
+  D;JEQ // Mini optimization, if the color value hasn't changed from the last time, we don't need to fill the screen.
+  @SCREENFILL
+  0;JMP
+
+(BLACK)
+  @color
+  M=-1 // Set the color value to black
+  D=D-M
+  @START
+  D;JEQ // Mini optimization, if the color value hasn't changed from the last time, we don't need to fill the screen.
+
+(SCREENFILL)
+  @offset
+  D=M
+  @SCREEN
+  D=D+A // Offset the address
+  @screenpointer
+  M=D // Write the address to a pointer variable
+  @color
+  D=M
+  @screenpointer
+  A=M
+  M=D // Write a value to the offset address
+
+  @numrows
+  D=M   // Set D to the numrows (so we can subtract)
+  @offset
+  M=M+1 // Increment the offset
+  D=D-M
+  @SCREENFILL
+  D;JNE // Keep filling until we hit our limit
+
+  @START
+  0;JMP // Back to the start to loop.
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -5,5 +5,36 @@
 
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+// R0 * R1 means we can sum up R0 + R0 ... + R0 R1 times to obtain a result.
 
-// Put your code here.
+  @i
+  M=0 // i=0
+  @R2
+  M=0 // R2=0
+
+(LOOP)
+  @i
+  D=M
+
+  @R0
+  D=D-M // D = i - R0
+
+  @END
+  D;JEQ
+  // Jump when this value is 0
+
+  @R1
+  D=M // D=R1
+
+  @R2
+  M=D+M // Add R1 to the current value in R2
+
+  @i
+  M=M+1 // Increment i
+
+  @LOOP
+  0;JMP // Back to the beginning of the loop
+
+(END)
+  @END
+  0;JMP // Infinite loop
